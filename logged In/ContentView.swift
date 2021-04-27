3b7c877bd9c2d53363b522bfcb2acb47aea7d38d
shahriarRahmanShaon <shaon463@gmail.com>
2021-04-27 23:02:28 +0600

    Initial Commit

---
+++ logged In/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  logged In
+//
+//  Created by sergio shaon on 27/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ logged In/logged_InApp.swift
@@ -0,0 +1,17 @@
+//
+//  logged_InApp.swift
+//  logged In
+//
+//  Created by sergio shaon on 27/4/21.
+//
+
+import SwiftUI
+
+@main
+struct logged_InApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
